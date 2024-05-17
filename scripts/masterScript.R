ead0652986b4bf28603f34de1c8832757de4973e
Your Name <your.email@example.com>
2024-05-17 11:51:58 -0400

    Added data from sampling on 9/20 to the chamberAdjustments1000 document

--- scripts/masterScript.R
+++ scripts/masterScript.R
@@ -2,18 +2,22 @@
 source("scripts/masterLibrary.R")
 
 # READ DATA
+
 source("scripts/readLgr.R")
 source("scripts/readFieldSheets.R")
 
 
 # Diffusive emissions
+
 source("scripts/chamberVolume.R") # calculate chamber headspace volume
 source("scripts/plotCleanLgr.R") # select time window for diffusive emission rate modeling
 source("scripts/analysis/calculateDiffusion.R") # diffusive emission rates.  
 
 # Ebullition rates
+
 source("scripts/analysis/ebullitionMassFluxFunction.R") # source function
 source("scripts/analysis/calculateEbullition.R") # eb_results
 
 # Merge diffusive and ebulitive rates --> calculate total
+
 source("scripts/analysis/calculateTotalEmissions.R")
