e3033b94f8d7f9b2083fe71323d8fbaf6150a526
Abdel767 <145685694+Abdel767@users.noreply.github.com>
2026-02-11 18:55:15 -0400

    Added new incubation data folder
    The file is containing the incubation data and the distance from the tributary is called "IncRates_vs_DistanceFromTributaries".
    
    You can find the file under the IncubationData folder

---
+++ scripts/masterLibrary.R
@@ -0,0 +1,15 @@
+#Load Project libraries
+library(tidyverse)
+library(tictoc)
+library(scales)
+library(plotly)
+library(janitor)
+library(fs)
+library(readxl)
+library(sf)
+library(mapview)
+library(leaflet)
+library(spsurvey)
+library(gridExtra)
+library(minpack.lm)
+
